88e5a66194750351a3aca4b731a76f5382bf9f36
Kelly Barber <Kelly@Kellys-MacBook-Air.local>
2019-03-03 23:04:18 -0500

    # Finish: Lecture 2.08
    # Details:
    Finished the bar chart exercise
    with data visualizations for
    worlds tallest buildings

--- lecture-2.08/js/main.js
+++ lecture-2.08/js/main.js
@@ -3,3 +3,32 @@
 *    Mastering Data Visualization with D3.js
 *    2.8 - Activity: Your first visualization!
 */
+
+(async () => {
+  const data = await d3.json('data/buildings.json')
+  data.forEach(point => point.height = +point.height)
+
+  const svg = d3.select('#chart-area')
+    .append('svg')
+    .attr('height', 500)
+    .attr('width', 500)
+
+  const bars = svg.selectAll('rect')
+    .data(data)
+    .enter()
+
+  bars.append('rect')
+    .attr('x', (data, index) => index * 40)
+    .attr('y', 0)
+    .attr('height', data => data.height)
+    .attr('width', 20)
+    .attr('fill', 'grey')
+
+
+
+})();
+
+
+// d3.json('data/buildings.json').then(data => {
+//   console.log(data)
+// })
